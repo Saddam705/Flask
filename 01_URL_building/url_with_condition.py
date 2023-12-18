f650650c1116a18fea413a86d51e365a0421d944
Saddam705 <110493839+Saddam705@users.noreply.github.com>
2023-12-18 06:00:16 +0000

    mutiple pages with some conditions

--- 01_URL_building/url_with_condition.py
+++ 01_URL_building/url_with_condition.py
@@ -1,1 +1,26 @@
-from flask
+from flask import Flask,url_for,request,redirect
+app = Flask(__name__)
+
+@app.route('/admin')
+def admin():
+    return 'this is a admin pag where you can learn somany things'
+
+@app.route('/librarion')
+def librarion():
+    return 'this is library page'
+
+@app.route('/student')
+def student():
+    return 'this is the student portal'
+
+@app.route('/user/<name>')
+def user(name):
+    if name=='admin':
+        return redirect(url_for('admin'))
+    elif name== 'librarion':
+        return redirect(url_for('librarion'))
+    else:
+        return redirect(url_for('student'))
+
+if __name__=='__main__':
+    app.run(debug=True)
